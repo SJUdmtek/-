a82e5ca8ecd2de9097ddf83b5113a7cc00e1741a
SJUdmtek <117709496+SJUdmtek@users.noreply.github.com>
2023-02-10 15:54:40 +0900

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import LinearProgress, {
+  LinearProgressProps,
+} from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import ModalBase from './ModalBase';
+
+function LinearProgressWithLabel(
+  props: LinearProgressProps & { value: number; time: number }
+) {
+  return (
+    <React.Fragment>
+      <div>바꾸시겠습니까?</div>
+
+      <LinearProgress
+        variant="determinate"
+        {...props}
+        sx={{ width: '500px', height: '20px' }}
+      />
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row-reverse',
+          marginTop: '10px',
+          marginRight: '5px',
+        }}
+      >{`${props.time}초 후 자동 거절...`}</div>
+    </React.Fragment>
+  );
+}
+
+export default function App() {
+  const [progress, setProgress] = React.useState(0);
+  const [time, setTime] = React.useState(5);
+  const [modalOpen, setModalOpen] = React.useState(false);
+
+  const showModal = () => {
+    setModalOpen(true);
+  };
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 0 : prevProgress + 20
+      );
+      setTime((prevTime) => (prevTime <= 0 ? 5 : prevTime - 1));
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [modalOpen]);
+
+  return (
+    <React.Fragment>
+      <button
+        onClick={() => {
+          showModal();
+        }}
+      >
+        모달 띄우기
+      </button>
+      {modalOpen && (
+        <ModalBase
+          onCloseClick={() => {
+            setModalOpen(false);
+            setProgress(0);
+            setTime(5);
+          }}
+        >
+          <Box sx={{ width: '100%' }}>
+            <LinearProgressWithLabel
+              value={progress}
+              time={time}
+              color="success"
+            />
+          </Box>
+        </ModalBase>
+      )}
+    </React.Fragment>
+  );
+}
---
+++ index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    {/* <StyledEngineProvider injectFirst> */}
+    <App />
+    {/* </StyledEngineProvider> */}
+  </StrictMode>
+);
